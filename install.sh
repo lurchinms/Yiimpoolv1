cfc0f1a42cdd2e27c13551d761b917ea75e077ba
Ronald Lonnborg Jr <kriptokyng@gmail.com>
2026-06-26 11:46:53 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@
 #########################################################
 
 # Default version tag if not provided as environment variable
-TAG=${TAG:-v2.7.5}
+TAG=${TAG:-v2.7.4}
 
 # File paths
 YIIMPOOL_VERSION_FILE="/etc/yiimpoolversion.conf"
